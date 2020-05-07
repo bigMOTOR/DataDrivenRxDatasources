bf956a66e825d52e96825b1eeb9816a5aef9d8ed
Dmitry Dovgan <dmitriy.dovgan@vistajet.com>
2020-05-07 14:06:28 +0300

    Add empty example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Dmitriy on 5/7/20.
+//  Copyright © 2020 Dmitriy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
