ed586b7d1264e706f79aa6464a3c6cbdd89cca98
mashe <scissorsinahead@mail.ru>
2021-08-26 10:14:42 +0300

    SectionHeaderFooterView Examples added

--- Example/Example/TableVIew/HeadersAndFootersViewModel.swift
+++ Example/Example/TableVIew/HeadersAndFootersViewModel.swift
@@ -5,44 +5,76 @@
 //  Created by Nikolay Fiantsev on 31.07.2020.
 //
 
+import UIKit
 import Foundation
 import RxCocoa
 import RxDataSources
 import DataDrivenRxDatasources
 
 struct HeadersAndFootersViewModel {
-  let sections: Driver<[AnimatableTableSectionModel<HeaderFooterSection>]>
+  enum Section: String {
+    case identity
+    case headerTitle
+    case footerTitle
+    case headerViewFooterView
+  }
+  
+  let sections: Driver<[DiffableTableSectionModel<HeaderFooterSection<Section>>]>
 
   init() {
-    let cells: [IdentifiableCellViewModel] = [ClassCellViewModel(value: "Some Cell", onSelected: { print("cell selected") })]
+    let cells: () -> [IdentifiableCellViewModel] = {
+      [ClassCellViewModel(value: UUID().uuidString, onSelected: { print("cell selected") })]
+    }
     
     self.sections = .just([
-      AnimatableTableSectionModel(model: HeaderFooterSection(identity: UUID().uuidString), items: cells),
-      AnimatableTableSectionModel(model: HeaderFooterSection(header: "Some Header"), items: cells),
-      AnimatableTableSectionModel(model: HeaderFooterSection(footer: "Some Footer"), items: cells),
-      AnimatableTableSectionModel(model: HeaderFooterSection(header: "Some Header", footer: "Some Footer"), items: cells)
+      DiffableTableSectionModel(
+        model: HeaderFooterSection(identity: .identity),
+        items: cells()
+      ),
+      DiffableTableSectionModel(
+        model: HeaderFooterSection(
+          identity: .headerTitle,
+          header: .title("Header title")
+        ),
+        items: cells()
+      ),
+      DiffableTableSectionModel(
+        model: HeaderFooterSection(
+          identity: .footerTitle,
+          footer: .title("Footer title")
+        ),
+        items: cells()
+      ),
+      DiffableTableSectionModel(
+        model: HeaderFooterSection(
+          identity: .headerViewFooterView,
+          header: .view(XibHeaderFooterViewModel(value: "Header view")),
+          footer: .view(XibHeaderFooterViewModel(value: "Footer view"))
+        ),
+        items: cells()
+      )
     ])
   }
 }
 
-struct HeaderFooterSection: SectionHeaderTitleType, SectionFooterTitleType {
-  let sectionHeaderTitle: String?
-  let sectionFooterTitle: String?
-  private var _identity: String
+struct HeaderFooterSection<Identity: Hashable>: SectionHeaderFooterType {
+  let sectionHeader: HeaderFooterContent?
+  let sectionFooter: HeaderFooterContent?
+  let identity: Identity
   
-  init(header: String? = nil, footer: String? = nil) {
-    self.sectionHeaderTitle = header
-    self.sectionFooterTitle = footer
-    self._identity = (sectionHeaderTitle ?? "") + (sectionFooterTitle ?? "")
-  }
-  
-  init(identity: String) {
-    self._identity = identity
-    self.sectionHeaderTitle = nil
-    self.sectionFooterTitle = nil
+  init(identity: Identity, header: HeaderFooterContent? = nil, footer: HeaderFooterContent? = nil) {
+    self.identity = identity
+    self.sectionHeader = header
+    self.sectionFooter = footer
   }
 }
 
-extension HeaderFooterSection: IdentifiableType {
-  var identity: String {  return _identity }
+extension HeaderFooterSection: IdentifiableType, Hashable {
+  static func == (lhs: HeaderFooterSection, rhs: HeaderFooterSection) -> Bool {
+    return lhs.identity == rhs.identity
+  }
+  
+  func hash(into hasher: inout Hasher) {
+    identity.hash(into: &hasher)
+  }
 }
